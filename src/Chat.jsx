abc77309d77809f8a70345e42707fbc342b8f5bb
R-Sidhartha <rsidhartha20@iitk.ac.in>
2024-01-16 20:52:22 +0530

    changes

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -19,7 +19,7 @@ export default function Chat() {
     connectToWs();
   }, [selectedUserId]);
   function connectToWs() {
-    const ws = new WebSocket('ws://papidtalkserver.onrender.com');
+    const ws = new WebSocket('wss://papidtalkserver.onrender.com');
     setWs(ws);
     ws.addEventListener('message', handleMessage);
     ws.addEventListener('close', () => {
