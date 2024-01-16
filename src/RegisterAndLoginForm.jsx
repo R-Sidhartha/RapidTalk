1838bfac044ba81fa7c0f355c8b4ba5ed3c5131b
R-Sidhartha <rsidhartha20@iitk.ac.in>
2024-01-16 20:41:56 +0530

    changes

--- src/RegisterAndLoginForm.jsx
+++ src/RegisterAndLoginForm.jsx
@@ -1,53 +1,118 @@
-import {useContext, useState} from "react";
+import { useContext, useState } from "react";
 import axios from "axios";
-import {UserContext} from "./UserContext.jsx";
+import { UserContext } from "./UserContext.jsx";
 
 export default function RegisterAndLoginForm() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [isLoginOrRegister, setIsLoginOrRegister] = useState('login');
-  const {setUsername:setLoggedInUsername, setId} = useContext(UserContext);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoginOrRegister, setIsLoginOrRegister] = useState("login");
+  const { setUsername: setLoggedInUsername, setId } = useContext(UserContext);
   async function handleSubmit(ev) {
     ev.preventDefault();
-    const url = isLoginOrRegister === 'register' ? 'register' : 'login';
-    const {data} = await axios.post(url, {username,password});
+    const url = isLoginOrRegister === "register" ? "register" : "login";
+    const { data } = await axios.post(url, { username, password });
     setLoggedInUsername(username);
     setId(data.id);
   }
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
   return (
-    <div className="bg-blue-50 h-screen flex items-center">
-      <form className="w-64 mx-auto mb-12" onSubmit={handleSubmit}>
-        <input value={username}
-               onChange={ev => setUsername(ev.target.value)}
-               type="text" placeholder="username"
-               className="block w-full rounded-sm p-2 mb-2 border" />
-        <input value={password}
-               onChange={ev => setPassword(ev.target.value)}
-               type="password"
-               placeholder="password"
-               className="block w-full rounded-sm p-2 mb-2 border" />
-        <button className="bg-blue-500 text-white block w-full rounded-sm p-2">
-          {isLoginOrRegister === 'register' ? 'Register' : 'Login'}
-        </button>
-        <div className="text-center mt-2">
-          {isLoginOrRegister === 'register' && (
-            <div>
-              Already a member?
-              <button className="ml-1" onClick={() => setIsLoginOrRegister('login')}>
-                Login here
+    <>
+      <div
+        className={`min-h-screen signuppage md:flex-row-reverse justify-around flex  sm:flex-col-reverse sm:items-center`}
+      >
+        <div
+          className={` flex flex-col items-center mx-10 sm:w-3/4 mt-2 md:w-1/3 my-4`}
+        >
+          <div className={`flex flex-col sm:w-screen md:w-full items-center`}>
+            <h1 className="text-2xl font-semibold w-2/3 sm:mx-1 text-center my-2">
+              Welcome To RapidTalk
+            </h1>
+            <h1 className={`text-2xl font-semibold w-2/3 sm:mx-1 text-center`}>
+            {isLoginOrRegister === "register" ? "Register" : "Login"}
+            </h1>
+          </div>
+          <div className={`sm:w-full md:w-3/4 mt-5`}>
+            <form class=" w-full " onSubmit={handleSubmit}>
+              <div class="mb-3 flex flex-col">
+                <label for="name" class="flex font-medium text-gray-700">
+                  UserName
+                </label>
+                <input
+                  type="text"
+                  class="form-input px-1 border border-gray-400 border-opacity-60 block w-full h-9 mt-3 rounded-md shadow-sm focus:ring focus:ring-opacity-50 focus:ring-blue-500 focus:outline-none focus:border-blue-300 text-sm"
+                  id="name"
+                  name="name"
+                  placeholder="Username"
+                  value={username}
+                  onChange={(ev) => setUsername(ev.target.value)}
+                  minLength={3}
+                  required
+                />
+              </div>
+  
+              <div class="mt-5 mb-3 flex flex-col">
+                <label for="password" class="flex font-medium text-gray-700">
+                  Password
+                </label>
+                <div className="relative">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    class="form-input border border-gray-400 border-opacity-60 block w-full h-9  px-1 mt-3 rounded-md shadow-sm focus:ring focus:ring-opacity-50 focus:ring-blue-500 focus:outline-none focus:border-blue-300 text-sm"
+                    id="password"
+                    name="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(ev) => setPassword(ev.target.value)}
+                    minLength={3}
+                    required
+                  />
+                  <button
+                    onClick={togglePasswordVisibility}
+                    className="absolute mt-1 top-1/2 right-2 transform -translate-y-1/2 cursor-pointer opacity-40 hover:opacity-70"
+                  >
+                    <i class="fa-solid fa-eye"></i>
+                  </button>
+                </div>
+              </div>
+              <button
+                type="submit"
+                class={`btn block w-full mt-7 px-4 py-2 rounded-md bg-gradient-to-r from-blue-300 via-purple-500 to-blue-200 font-semibold hover:opacity-90 text-lg`}
+              >
+                {isLoginOrRegister === "register" ? "Register" : "Login"}
               </button>
+            <div className="text-center mt-2">
+              {isLoginOrRegister === "register" && (
+                <div>
+                  Already a member?
+                  <button
+                    className="ml-1"
+                    onClick={() => setIsLoginOrRegister("login")}
+                    >
+                    Login here
+                  </button>
+                </div>
+              )}
+              {isLoginOrRegister === "login" && (
+                <div>
+                  Dont have an account?
+                  <button
+                    className="ml-1"
+                    onClick={() => setIsLoginOrRegister("register")}
+                    >
+                    Register
+                  </button>
+                </div>
+              )}
             </div>
-          )}
-          {isLoginOrRegister === 'login' && (
-            <div>
-              Dont have an account?
-              <button className="ml-1" onClick={() => setIsLoginOrRegister('register')}>
-                Register
-              </button>
-            </div>
-          )}
+              </form>{" "}
+          </div>
         </div>
-      </form>
-    </div>
+      </div>
+    </>
   );
 }
+
